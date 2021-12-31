8c6ef4836f2dc7424c7dd026819ee0eec39e1b97
XIAO, YU-YI <45507258+myg36t91@users.noreply.github.com>
2022-01-01 00:27:40 +0800

    Update face_mesh.py

--- face_mesh.py
+++ face_mesh.py
@@ -7,6 +7,9 @@ cap = cv2.VideoCapture(0)
 mp_face_mesh = mp.solutions.face_mesh
 drawing_utils = mp.solutions.drawing_utils
 
+face_landmark_style = drawing_utils.DrawingSpec(color=(0, 0, 255), thickness=1)
+face_connect_style = drawing_utils.DrawingSpec(color=(255, 255, 255), thickness=3)
+
 face = mp_face_mesh.FaceMesh(static_image_mode=False, min_tracking_confidence=0.6, min_detection_confidence=0.6)
 
 while True:
@@ -17,7 +20,7 @@ while True:
         if face_result.multi_face_landmarks:
             for i in face_result.multi_face_landmarks:
                 # print(i.landmark[0].y*480)
-                drawing_utils.draw_landmarks(img, i, mp_face_mesh.FACEMESH_CONTOURS, landmark_drawing_spec=drawing_utils.DrawingSpec(color=(0, 0, 255), circle_radius=1))
+                drawing_utils.draw_landmarks(img, i, mp_face_mesh.FACEMESH_CONTOURS, face_landmark_style, face_connect_style)
 
         cv2.imshow('img', img)
 
